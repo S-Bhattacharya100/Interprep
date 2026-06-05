043e5bd3fbee3ffd1665adb049ee62f25b5c65a9
S Bhattacharya <swapnamoybhattacharya053@gmail.com>
2026-06-06 02:03:15 +0530

    Add Axios instance

---
+++ client/src/utils/axiosInstance.js
@@ -0,0 +1,20 @@
+const axios = require("axios");
+
+const axiosInstance = axios.create({
+    baseURL: "http://localhost:3000/api",
+    headers: {
+        "Content-Type": "application/json"
+    }
+});
+
+axiosInstance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("accessToken");
+
+    if(token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
+
+export default axiosInstance;
